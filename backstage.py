fe27e6d99aedaef6e97720ad1fed54926c23c248
Alex Navarro <alexjandron99@gmail.com>
2019-01-09 19:05:59 +0100

    Primeros casos test pasados

--- backstage.py
+++ backstage.py
@@ -4,4 +4,32 @@ from regular_item import RegularItem
 class Backstage(RegularItem):
 
 
-    def 
+    def update_quality(self):
+        if self.sell_in >= 10:
+            self.setQuality(+1)
+        elif self.sell_in >= 5:
+            self.setQuality(+2)
+        elif self.sell_in >= 0:
+            self.setQuality(+3)
+        else:
+            self.quality = 0
+        self.setSellIn()
+
+
+
+
+if __name__ == "__main__":
+    
+
+    backItem = Backstage("Backstage", 12, 23)
+    backItem.update_quality() 
+    assert backItem.getQuality() == 24
+
+    stage1 = Backstage("Backstage", 5, 15)
+    stage1.update_quality()
+    assert stage1.getQuality() == 17
+
+    stageBack = Backstage("Backstage", 0, 24)
+    stageBack.update_quality()
+    assert stageBack.getQuality() == 27
+
