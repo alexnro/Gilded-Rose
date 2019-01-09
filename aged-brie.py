dffb48138244f05e2175b9b813596daf1b71408d
Alex Navarro <alexjandron99@gmail.com>
2019-01-09 17:26:10 +0100

    Aged Brie funcional

--- aged-brie.py
+++ aged-brie.py
@@ -1,5 +1,3 @@
-from item import Item
-from updateable import Interface
 from regular_item import RegularItem
 
 
@@ -8,13 +6,20 @@ class AgedBrie(RegularItem):
 
     def __init__(self, name, sell_in, quality):
         RegularItem.__init__(self, name, sell_in, quality)
-    
+
+
+    def update_quality(self):
+        if RegularItem.getSellIn(self) > 0:
+            self.setQuality(+1)
+        else:
+            self.setQuality(+2)
+        self.setSellIn
 
 
 if __name__ == "__main__":
     
 
     quesito = AgedBrie("Aged Brie", 2, 0)
-    assert quesito.getName == "Aged Brie"
+    assert quesito.getName() == "Aged Brie"
     quesito.update_quality()
-    assert quesito.getQuality == -2
+    assert quesito.getQuality() == 1
